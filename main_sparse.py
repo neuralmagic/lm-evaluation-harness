14e54d85e9fd0b4f268f719c9a865322f164ed69
Alexandre Marques <alexandre@neuralmagic.com>
2023-09-13 15:15:40 -0400

    Remove print of undefined arguments

--- main_sparse.py
+++ main_sparse.py
@@ -105,7 +105,7 @@ def main():
 
     batch_sizes = ",".join(map(str, results["config"]["batch_sizes"]))
     print(
-        f"{args.model} ({args.model_args}), limit: {args.limit}, provide_description: {args.provide_description}, "
+        f"limit: {args.limit}, provide_description: {args.provide_description}, "
         f"num_fewshot: {args.num_fewshot}, batch_size: {args.batch_size}{f' ({batch_sizes})' if batch_sizes else ''}"
     )
     print(evaluator.make_table(results))
